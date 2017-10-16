4501b86cf1cbb0d4c54f86fd852ecec22e728ef9
JoeBew42 <joebew42@gmail.com>
2017-10-16 20:31:06 +0200

    extract do_request method

--- test/plug_example/hello_router_test.exs
+++ test/plug_example/hello_router_test.exs
@@ -2,12 +2,11 @@ defmodule PlugExample.HellRouterTest do
   use ExUnit.Case, async: true
   use Plug.Test
 
-  @opts PlugExample.HelloRouter.init([])
+  @router PlugExample.HelloRouter
+  @opts @router.init([])
 
   test "returns hello world" do
-    conn = conn(:get, "/")
-
-    conn = PlugExample.HelloRouter.call(conn, @opts)
+    conn = do_request(:get, "/")
 
     assert :sent == conn.state
     assert 200 == conn.status
@@ -15,12 +14,15 @@ defmodule PlugExample.HellRouterTest do
   end
 
   test "returns a oops 404 when there is not matching endpoint" do
-    conn = conn(:get, "/not_existing")
-
-    conn = PlugExample.HelloRouter.call(conn, @opts)
+    conn = do_request(:get, "/not_existing")
 
     assert :sent == conn.state
     assert 404 == conn.status
     assert "oops" == conn.resp_body
+  end
+
+  defp do_request(verb, endpoint) do
+    conn(verb, endpoint)
+    |> @router.call(@opts)
   end
 end
